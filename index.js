bdd51bd72259230daa548e969fc5231bb1071daf
Levi Tomes <levi.tomes@agosto.com>
2017-12-15 17:21:46 -0600

    Initial commit, no polyfills or anything fancy supported yet.

---
+++ index.js
@@ -0,0 +1,12 @@
+export function get(k) {
+    let results = {};
+    for (v in localStorage) {
+        if (v.indexOf('firebase:authUser:') !== -1) {
+            results[v] = localStorage.getItem(v);
+        }
+        return results;
+    }
+}
+export function set(k, v) {
+    localStorage.setItem(k, v)
+}
